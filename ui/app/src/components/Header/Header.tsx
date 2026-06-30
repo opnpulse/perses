617b407fb12d8397f3872cb88008d9ba95f5d38b
RokibulHasan7 <mdrokibulhasan@appscode.com>
2026-06-30 11:29:10 +0600

    Fix panel is not visible for small screen <=600 px
    Signed-off-by: RokibulHasan7 <mdrokibulhasan@appscode.com>

--- ui/app/src/components/Header/Header.tsx
+++ ui/app/src/components/Header/Header.tsx
@@ -23,7 +23,7 @@ import { ToolMenu } from './ToolMenu';
 import { AccountMenu } from './AccountMenu';
 import { ThemeSwitch } from './ThemeSwitch';
 import { SearchBar } from './SearchBar/SearchBar';
-import BrandLogo from '../logo/BrandLogo';
+import AppscodeLogo from '../logo/AppscodeLogo';
 import { AppDrawer } from './AppDrawer';
 import { useCookies } from 'react-cookie';
 import { activeOrganization } from '../../constants/auth-token';
@@ -75,7 +75,7 @@ export default function Header(): JSX.Element {
               minWidth: 30,
             }}
           >
-            <BrandLogo height={30} width="auto" />
+            <AppscodeLogo height={30} width="auto" />
           </Button>
           {isLaptopSize && <ObserveLabel />}
         </Box>

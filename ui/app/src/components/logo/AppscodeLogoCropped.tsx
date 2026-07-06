82dd4ca77699c80202eec11f9ef2599eea4317a0
rafi <rafialam@appscode.com>
2026-07-06 16:14:47 +0600

    Make logo dynamic taking from branding
    Signed-off-by: rafi <rafialam@appscode.com>

--- ui/app/src/components/logo/AppscodeLogoCropped.tsx
+++ ui/app/src/components/logo/AppscodeLogoCropped.tsx
@@ -12,15 +12,16 @@
 // limitations under the License.
 
 import { ReactElement, ImgHTMLAttributes } from 'react';
-import appscodeLogoCropped from '../../../../assets/appscodeLogoCropped.png';
+import { useBranding } from '../../model/branding-client';
 
 interface AppscodeLogoCroppedProps extends ImgHTMLAttributes<HTMLImageElement> {
   title?: string;
 }
 
 function AppscodeLogoCropped(props: AppscodeLogoCroppedProps): ReactElement {
-  const { title = 'Appscode Logo', width = '130', height = '30', ...rest } = props;
-  return <img src={appscodeLogoCropped} alt={title} width={width} height={height} {...rest} />;
+  const { title = 'Brand Logo', width = '130', height = '30', ...rest } = props;
+  const { data: branding } = useBranding();
+  return <img src={branding?.logo} alt={title} width={width} height={height} {...rest} />;
 }
 
 export default AppscodeLogoCropped;
